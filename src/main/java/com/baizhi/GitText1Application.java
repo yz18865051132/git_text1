d1fc5bfbb064d4143f25bba9529f25610a8e7a96
YuZun <yz18865051132>
2021-06-07 19:52:41 +0800

    第一次搭建环境

---
+++ src/main/java/com/baizhi/GitText1Application.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitText1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitText1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/baizhi/GitText1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitText1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
